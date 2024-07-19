778cff74d8373d82f584c7897b49c0d71d0e07f1
Andrew <andrewchou949@gmail.com>
2024-07-18 19:00:59 -0700

    Worked on isAnagram problem

--- 1. Arrays/Solution.py
+++ 1. Arrays/Solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solution:
     
     # return true if any value exist more than once
@@ -7,7 +9,15 @@ class Solution:
             return False
         return True
     
+    # return True if it's anagram
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return false immediately when len is no equivalent
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
+        
+    
     
 run = Solution()
-print(run.hasDuplicate([-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20]))
-
+print(run.hasDuplicate([1, 2, 3]))
+print(run.isAnagram("racecar", "carrace"))
