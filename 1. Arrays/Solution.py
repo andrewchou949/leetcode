b357122d24eb9e93d7f33bd8e4bb44f716369b46
Andrew <andrewchou949@gmail.com>
2024-07-18 19:07:08 -0700

    Added another solution for isAnagram

--- 1. Arrays/Solution.py
+++ 1. Arrays/Solution.py
@@ -11,13 +11,25 @@ class Solution:
     
     # return True if it's anagram
     def isAnagram(self, s: str, t: str) -> bool:
-        # return false immediately when len is no equivalent
+        # # return false immediately when len is no equivalent
+        # if len(s) != len(t):
+        #     return False
+        # return sorted(s) == sorted(t)
+        
+        # another solution
         if len(s) != len(t):
             return False
-        return sorted(s) == sorted(t)
+        my_s, my_t = defaultdict(int), defaultdict(int)
+        for i in range(len(s)):
+            my_s[s[i]] += 1
+            my_t[t[i]] += 1
+        for item in my_s.keys():
+            if my_s[item] != my_t[item]:
+                return False
+        return True
         
     
     
 run = Solution()
 print(run.hasDuplicate([1, 2, 3]))
-print(run.isAnagram("racecar", "carrace"))
+print(run.isAnagram("jar", "jam"))
