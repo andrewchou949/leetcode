41dfe8f266a723f2f3ced555ef78e58348fc848e
Andrew <andrewchou949@gmail.com>
2024-07-23 14:52:00 -0700

    Solved top k most frequent

--- 1. Arrays/Solution.py
+++ 1. Arrays/Solution.py
@@ -70,10 +70,29 @@ class Solution:
         return result
     
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        pass
+        # need to create an array to track the most frequently showned val
+        # the index is the count
+        # the value is the array that store the element
+        dummy = [[] for i in range(len(nums) + 1)]
+        my_dict = defaultdict(int) # store the count of each element
+        for item in nums:
+            my_dict[item] += 1
+        # loop through each pair of k and v
+        for n, v in my_dict.items():
+            dummy[v].append(n)
+        result = []
+        for i in range(len(dummy) - 1, 0, -1):
+            for n in dummy[i]:
+                result.append(n)
+                if len(result) == k:
+                    return result
+    
+        
+            
     
 run = Solution()
 print(run.hasDuplicate([1, 2, 3]))
 print(run.isAnagram("jar", "jam"))
 print(run.twoSum([3, 4, 5, 6], 11))
 print(run.groupAnagrams(["act","pots","tops","cat","stop","hat"]))
+print(run.topKFrequent([1, 2, 2, 3, 3, 3, 4], 2))
