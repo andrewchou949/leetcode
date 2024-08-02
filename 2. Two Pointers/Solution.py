93de702222f5b25672adb819ccf3fb8fec953d24
Andrew <andrewchou949@gmail.com>
2024-08-01 22:57:16 -0700

    Add isPalindrome question

---
+++ 2. Two Pointers/Solution.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    # ignore case sensitivity and non character
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = ""
+        for char in s:
+            if char.isalnum():
+                cleaned += char.lower()
+        left, right = 0, len(cleaned) - 1
+        while left <= right:
+            if cleaned[left] != cleaned[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+        
+
+run = Solution()
+print(run.isPalindrome("Was it a car or a cat I saw?"))
