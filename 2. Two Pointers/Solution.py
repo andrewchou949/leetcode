3ef7813af46f758b40b387e85edb73b7c541d496
Andrew <andrewchou949@gmail.com>
2024-08-06 21:17:31 -0700

    Finish some portion of threeSum

--- 2. Two Pointers/Solution.py
+++ 2. Two Pointers/Solution.py
@@ -34,8 +34,19 @@ class Solution:
     # return a list of triplet possibilities whose sum == 0
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        for item in nums:
-            pass
+        nums.sort() # sort the array first
+        # then loop through element, when one item is chosen, do twoSumII to solve and find combination
+        for i, item in enumerate(nums):
+            
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                three = nums[i] + nums[left] + nums[right]
+                if three > 0:
+                    right -= 1
+                elif three < 0:
+                    left += 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
         return res
         
         
@@ -46,3 +57,5 @@ run = Solution()
 print(run.isPalindrome("Was it a car or a cat I saw?"))
 # 2
 print(run.twoSum([1,2,3,4,5,6,7], 6))
+# 3
+print(run.threeSum([-1,0,1,2,-1,-4]))
